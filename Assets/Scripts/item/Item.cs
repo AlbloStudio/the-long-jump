52bdb063746e0ef1c10028ef26fee037db506171
AlexTemina <AlexTemina@gmail.com>
2023-01-29 00:16:18 +0100

    add static items (cannot be moved and safe area is not afffected)

--- Assets/Scripts/item/Item.cs
+++ Assets/Scripts/item/Item.cs
@@ -1,21 +1,39 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using static Enum;
 
 namespace Assets.Scripts.item
 {
     public class Item : MonoBehaviour
     {
+
+        [Tooltip("The color when in planning mode")]
+        [SerializeField] private Material _planningModeMaterial;
+
+        [Tooltip("Can the item be moved?")]
+        [SerializeField] private bool _isStatic = false;
+
         private Collider2D _safeArea;
         private Jumper _jumper;
+        private Renderer _renderer;
+        private Light2D _light;
 
         private Vector2 _originalPosition;
+        private Material _originalMaterial;
 
         private void Awake()
         {
             _originalPosition = transform.position;
             _jumper = GetComponent<Jumper>();
+            _renderer = GetComponent<Renderer>();
+            _light = GetComponent<Light2D>();
 
-            gameObject.SetActive(false);
+            _originalMaterial = _renderer.sharedMaterial;
+
+            if (!_isStatic)
+            {
+                gameObject.SetActive(false);
+            }
         }
 
         private bool IsInSafeArea()
@@ -27,14 +45,25 @@ namespace Assets.Scripts.item
         {
             _safeArea = safeArea;
 
-            gameObject.SetActive(true);
+            if (!_isStatic)
+            {
+                gameObject.SetActive(true);
+                _jumper.SetPlanningMode(PlanningMode.Planning);
+            }
 
-            _jumper.SetPlanningMode(PlanningMode.Planning);
+            if (_planningModeMaterial)
+            {
+                _renderer.sharedMaterial = _planningModeMaterial;
+                _light.enabled = true;
+            }
         }
 
         public void ExitPlanningMode()
         {
-            if (IsInSafeArea())
+            _renderer.sharedMaterial = _originalMaterial;
+            _light.enabled = false;
+
+            if (IsInSafeArea() || _isStatic)
             {
                 _jumper.SetPlanningMode(PlanningMode.Playing);
             }
