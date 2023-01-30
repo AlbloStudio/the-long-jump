01069a7b5a92cbe830f5440e8466bd0424b1f148
AlexTemina <AlexTemina@gmail.com>
2023-01-30 01:00:41 +0100

    water shader, still some touches

--- Assets/Scripts/utils/MeshGenerator.cs
+++ Assets/Scripts/utils/MeshGenerator.cs
@@ -6,6 +6,7 @@ public class MeshGenerator : MonoBehaviour
     [SerializeField] private Vector2 _planeSize = new(1, 1);
     [SerializeField] private Vector2 _offset = Vector2.zero;
     [SerializeField] private int _planeResolution = 1;
+    [SerializeField] private bool auto = false;
 
     public Vector2 PlaneSize { get => _planeSize; set => _planeSize = value; }
     public Vector2 Offset { get => _offset; set => _offset = value; }
@@ -22,7 +23,13 @@ public class MeshGenerator : MonoBehaviour
         _mesh = new Mesh();
         _meshFilter = GetComponent<MeshFilter>();
 
+        if (auto)
+        {
+
+            GeneratePlane();
+        }
     }
+
     public void GeneratePlane()
     {
         _vertices.Clear();
@@ -33,7 +40,6 @@ public class MeshGenerator : MonoBehaviour
 
         GenerateVertices(resolution);
         GenerateTriangles(resolution);
-        GenerateUV(resolution);
 
         _meshFilter.mesh = _mesh;
     }
@@ -48,10 +54,13 @@ public class MeshGenerator : MonoBehaviour
             for (int x = 0; x <= resolution; x++)
             {
                 _vertices.Add(new Vector3(x * xPerStep, y * yPerStep, transform.position.z) + (Vector3)_offset);
+                _uv.Add(new Vector2(x / (float)resolution, y / (float)resolution));
+
             }
         }
 
         _mesh.vertices = _vertices.ToArray();
+        _mesh.uv = _uv.ToArray();
     }
 
     private void GenerateTriangles(int resolution)
@@ -74,22 +83,6 @@ public class MeshGenerator : MonoBehaviour
         }
 
         _mesh.triangles = _triangles.ToArray();
-    }
-
-    private void GenerateUV(int resolution)
-    {
-        for (int row = 0; row < resolution; row++)
-        {
-            for (int column = 0; column < resolution; column++)
-            {
-                _uv.Add(new(row, column));
-                _uv.Add(new(row, column + 1));
-                _uv.Add(new(row + 1, column));
-                _uv.Add(new(row + 1, column + 1));
-            }
-        }
-
-        _mesh.uv = _uv.ToArray();
     }
 }
 
