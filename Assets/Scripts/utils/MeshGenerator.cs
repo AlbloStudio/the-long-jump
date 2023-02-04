59fbe6a3b4a45465454323bb71707b92b2ac3c6b
AlexTemina <AlexTemina@gmail.com>
2023-02-05 00:52:25 +0100

    remove mesh generatpr update since it's not really used

--- Assets/Scripts/utils/MeshGenerator.cs
+++ Assets/Scripts/utils/MeshGenerator.cs
@@ -7,27 +7,17 @@ public class MeshGenerator : MonoBehaviour
     [SerializeField] private Vector2 _offset = Vector2.zero;
     [SerializeField] private int _planeResolution = 1;
     [SerializeField] private bool _auto = false;
-    [SerializeField] private bool _renerateOnPlay = false;
 
     public Vector2 PlaneSize { get => _planeSize; set => _planeSize = value; }
     public Vector2 Offset { get => _offset; set => _offset = value; }
     public int PlaneResolution { get => _planeResolution; set => _planeResolution = value; }
     public bool Auto { get => _auto; set => _auto = value; }
-    public bool RegenerateOnPlay { get => _renerateOnPlay; set => _renerateOnPlay = value; }
 
     public MeshFilter ObjectMeshFilter { get; private set; }
 
     private void Awake()
     {
         ObjectMeshFilter = GetComponent<MeshFilter>();
-    }
-
-    private void Update()
-    {
-        if (_renerateOnPlay)
-        {
-            GeneratePlane();
-        }
     }
 
     public void GeneratePlane()
