c432d31afc7a94df9d9c2fb83c740f52c2e30f50
AlexTemina <AlexTemina@gmail.com>
2023-02-22 23:34:32 +0100

    improve cloud fog

--- Assets/_Features/Environment/Clouds/Cloud.cs
+++ Assets/_Features/Environment/Clouds/Cloud.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class Cloud : MonoBehaviour
 {
     [SerializeField] private Vector2 _velocityRange = new(2f, 0.5f);
+    [SerializeField] private Vector2 _sizeRange = new(1f, 5f);
+
+    private ParticleSystem _particles;
 
     private void Awake()
     {
-        ParticleSystem particles = GetComponent<ParticleSystem>();
+        _particles = GetComponent<ParticleSystem>();
+        ReCalculate();
+    }
 
-        ParticleSystem.VelocityOverLifetimeModule vel = particles.velocityOverLifetime;
+    public void ReCalculate()
+    {
+        ParticleSystem.VelocityOverLifetimeModule vel = _particles.velocityOverLifetime;
         vel.x = Random.Range(-_velocityRange.x, _velocityRange.x);
         vel.y = Random.Range(-_velocityRange.y, _velocityRange.y);
+
+        ParticleSystem.MainModule shape = _particles.main;
+        shape.startSize = Random.Range(_sizeRange.x, _sizeRange.y);
     }
 }
--- Assets/_Features/Environment/Clouds/Clouds.cs
+++ Assets/_Features/Environment/Clouds/Clouds.cs
@@ -5,25 +5,64 @@ using UnityEngine;
 public class Clouds : MonoBehaviour
 {
     [SerializeField] private float _timeForNewCloud = 4;
-    [SerializeField] private ParticleSystem _clouds;
+    [SerializeField] private float _cloudLife = 20;
+    [SerializeField] private Cloud _cloudPrefab;
+    [SerializeField] private Vector2 zRange = new(3f, 8f);
 
     private float _timeCount = 0;
     private Collider2D _spawnArea;
+    private Cloud[] _cloudInstances;
+    private int _index = 0;
+    private int _cloudAmount;
 
     private void Awake()
     {
         _spawnArea = GetComponent<Collider2D>();
+        _cloudAmount = Mathf.CeilToInt(_cloudLife / _timeForNewCloud);
+
+        _cloudInstances = new Cloud[_cloudAmount];
     }
 
     private void Update()
     {
         _timeCount += Time.deltaTime;
 
-        if (_timeCount > +_timeForNewCloud)
+        if (_timeCount >= _timeForNewCloud)
         {
-            var cloud = Instantiate(_clouds, Geometry.GetRandomPointInArea(_spawnArea), Quaternion.identity);
-            GameObject.Destroy(cloud.gameObject, 20);
+            if (_cloudInstances[_index] == null)
+            {
+                _cloudInstances[_index] = MakeCloud();
+            }
+            else
+            {
+                _cloudInstances[_index].transform.position = GetNewPosition();
+                _cloudInstances[_index].ReCalculate();
+            }
+
             _timeCount = 0;
+            _index++;
         }
+
+        if (_index == _cloudAmount)
+        {
+            _index = 0;
+        }
+    }
+
+    private Cloud MakeCloud()
+    {
+        Cloud cloud = Instantiate(_cloudPrefab, GetNewPosition(), Quaternion.identity);
+        ParticleSystem.MainModule main = cloud.GetComponent<ParticleSystem>().main;
+        main.startLifetime = _cloudLife;
+
+        return cloud;
+    }
+
+    private Vector3 GetNewPosition()
+    {
+        Vector2 new2DPosition = Geometry.GetRandomPointInArea(_spawnArea);
+        float z = Random.Range(zRange.x, zRange.y);
+
+        return new(new2DPosition.x, new2DPosition.y, z);
     }
 }
