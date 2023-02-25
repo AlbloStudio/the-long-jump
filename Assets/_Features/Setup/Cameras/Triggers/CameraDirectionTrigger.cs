5fbd817cbcad702fc784a03df2f8df07489cf9b7
AlexTemina <AlexTemina@gmail.com>
2023-02-25 23:55:22 +0100

    new way of changing cameras

--- Assets/_Features/Setup/Cameras/Triggers/CameraDirectionTrigger.cs
+++ Assets/_Features/Setup/Cameras/Triggers/CameraDirectionTrigger.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class CameraDirectionTrigger : MonoBehaviour
 {
-    [SerializeField] private Vector2 direction;
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera NewCamera;
 
     private void OnTriggerEnter2D(Collider2D collided)
     {
         if (collided.transform.Equals(GeneralData.Instance.Player.transform))
         {
-            GeneralData.Instance.MainCameraLeft.enabled = direction.x < 0;
+            Cinemachine.CinemachineVirtualCamera currentVirtualCamera = Cinemachine.CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera as Cinemachine.CinemachineVirtualCamera;
+            currentVirtualCamera.enabled = false;
+
+            NewCamera.enabled = true;
         }
     }
 }
